fb9f178f08a27f980f3d8b7828c766eac77374a9
后端 <>
2020-11-06 12:55:01 +0800

    no message

--- quant_run/tests.py
+++ quant_run/tests.py
@@ -2,26 +2,6 @@
 
 
 if __name__ == '__main__':
-    from vnpy.app.cta_strategy.backtesting import BacktestingEngine
-    from vnpy.app.cta_strategy.strategies.fx_strategy import FxStrategy
-    from app.cta_strategy.strategies.average_strategy import AverageStrategy
-    from trader.constant import Interval
-    from datetime import datetime
-    engine = BacktestingEngine()
-    engine.set_parameters(
-        vt_symbol="BTCUSDT.BINANCE",
-        interval=Interval.HOUR.value,
-        start=datetime(2017, 1, 1),
-        end=datetime(2020, 11, 1),
-        rate=2.5 / 10000,
-        slippage=0.2,
-        size=100,
-        pricetick=0.2,
-        capital=1000000,
-    )
-    engine.add_strategy(AverageStrategy, {})
-    engine.load_data()
-    engine.run_backtesting()
-    df = engine.calculate_result()
-    engine.calculate_statistics()
-    engine.show_chart()
+    from coinmarketcap import Market
+    coinmarketcap = Market()
+    coinmarketcap.listings()
--- vnpy/app/cta_strategy/base.py
+++ vnpy/app/cta_strategy/base.py
@@ -1,6 +1,8 @@
 """
 Defines constants and objects used in CtaStrategy App.
 """
+import calendar
+import datetime
 
 from dataclasses import dataclass, field
 from enum import Enum
@@ -45,7 +47,7 @@ class StopOrder:
 EVENT_CTA_LOG = "eCtaLog"
 EVENT_CTA_STRATEGY = "eCtaStrategy"
 EVENT_CTA_STOPORDER = "eCtaStopOrder"
-
+now_date = datetime.datetime.now()
 
 INTERVAL_DELTA_MAP = {
     Interval.MINUTE: timedelta(minutes=1),
@@ -54,5 +56,13 @@ INTERVAL_DELTA_MAP = {
     Interval.MINUTE_15: timedelta(minutes=15),
     Interval.MINUTE_30: timedelta(minutes=30),
     Interval.HOUR: timedelta(hours=1),
+    Interval.HOUR_2: timedelta(hours=2),
+    Interval.HOUR_4: timedelta(hours=4),
+    Interval.HOUR_6: timedelta(hours=6),
+    Interval.HOUR_8: timedelta(hours=8),
+    Interval.HOUR_12: timedelta(hours=12),
     Interval.DAILY: timedelta(days=1),
+    Interval.DAILY_3: timedelta(days=3),
+    Interval.WEEKLY: timedelta(weeks=1),
+    Interval.MONTH: timedelta(days=calendar.monthrange(now_date.year,now_date.month)[1]),
 }
